e4503442d18d30ee55251e316573160e567ce6fe
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-09-10 22:57:39 -0500

    add create user route

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -1,16 +1,20 @@
 package router
 
 import (
+	"ambrosia-zeus-api/cmd/api/handler"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 )
 
-func SetupEndpoints() {
-	router := gin.New()
+func SetupEndpoints(db *gorm.DB) {
+	router := gin.Default()
 
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, "pong")
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "pong")
 	})
+
+	router.POST("/users/create", handler.CreateUserHandler{DB: db}.CreateUser)
 
 	err := router.Run()
 	if err != nil {
