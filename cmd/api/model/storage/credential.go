788bb6247c5026f7c967f88f76ca0ece4f2a120a
Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co>
2024-05-31 06:50:15 -0500

    credential

--- cmd/api/model/storage/credential.go
+++ cmd/api/model/storage/credential.go
@@ -1,23 +1,23 @@
 package storage
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Credential struct {
-	ID             string
-	Value          string
-	LatestPassword string
+	ID    string
+	Value string
 }
 
 func NewCredential(credential string) Credential {
 	hash := hashCredential(credential)
 
 	return Credential{
-		ID:             uuid.NewString(),
-		Value:          hash,
-		LatestPassword: "",
+		ID:    uuid.NewString(),
+		Value: hash,
 	}
 }
 
